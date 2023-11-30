31d0c50fb302b54e0aeb0bc676ac71b1640612ee
Jay Miller <kjaymiller@gmail.com>
2023-11-30 14:38:40 -0500

    adds flask

--- src/flaskapp/config/production.py
+++ src/flaskapp/config/production.py
@@ -2,15 +2,18 @@ import os
 
 DEBUG = False
 
-
 if "WEBSITE_HOSTNAME" in os.environ:
     ALLOWED_HOSTS = [os.environ["WEBSITE_HOSTNAME"]]
 else:
     ALLOWED_HOSTS = []
 
-
 dbuser = os.environ["POSTGRES_USERNAME"]
 dbpass = os.environ["POSTGRES_PASSWORD"]
 dbhost = os.environ["POSTGRES_HOST"]
 dbname = os.environ["POSTGRES_DATABASE"]
-DATABASE_URI = f"postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}"
+dbport = os.environ.get("POSTGRES_PORT", 5432)
+sslmode = os.environ.get("POSTGRES_SSL")
+DATABASE_URI = f"postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}:{dbport}/{dbname}"
+
+if sslmode:
+    DATABASE_URI = f"{DATABASE_URI}?sslmode={sslmode}"
