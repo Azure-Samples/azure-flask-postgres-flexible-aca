d1c99498bdc49841a59a6670c45d3c4ff4d6f33d
Jay Miller <kjaymiller@gmail.com>
2024-01-17 14:51:51 -0500

    Cruft Update

--- src/flaskapp/__init__.py
+++ src/flaskapp/__init__.py
@@ -1,12 +1,10 @@
 import os
 
 import click
+from azure.monitor.opentelemetry import configure_azure_monitor
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from opencensus.ext.azure.trace_exporter import AzureExporter
-from opencensus.ext.flask.flask_middleware import FlaskMiddleware
-from opencensus.trace.samplers import ProbabilitySampler
 from sqlalchemy.orm import DeclarativeBase
 
 
@@ -23,16 +21,11 @@ def create_app(test_config=None):
     app = Flask(__name__, static_folder="../static", template_folder="../templates")
 
     # Load configuration for prod vs. dev
+    if os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING"):
+        configure_azure_monitor()
     is_prod_env = "RUNNING_IN_PRODUCTION" in os.environ
     if not is_prod_env:
         app.config.from_object("flaskapp.config.development")
-    else:
-        app.config.from_object("flaskapp.config.production")
-        FlaskMiddleware(
-            app,
-            exporter=AzureExporter(connection_string=os.environ.get("APPLICATIONINSIGHTS_CONNECTION_STRING", None)),
-            sampler=ProbabilitySampler(rate=1.0),
-        )
 
     # Configure the database
     if test_config is not None:
